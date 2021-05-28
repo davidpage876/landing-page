1f01a3bab15898011aeaeb7ceb4dd9765fbfae52
David C. Page <evildavo@gmail.com>
2021-05-28 20:10:36 +1000

    feat: Add 'focus' class to sections in focus (in jS)

--- js/app.js
+++ js/app.js
@@ -1,5 +1,33 @@
 
 /**
+ * @description Makes the given section in focus.
+ * This triggers CSS state to highlight the section in the focus.
+ * @param {Element} section - Section to focus on.
+ * @param {Element[]} contentSections - List of all content sections.
+ * Used to remove focus from other sections.
+ * @param {Navigation} navigation - Navigation list to update to match.
+ */
+function focusSection(section, contentSections, navigation) {
+
+    // Remove focus from all sections.
+    for (const s of contentSections) {
+        s.classList.remove('focus');
+    }
+
+    // Remove focus from all nav items.
+    for (const item of navigation.items) {
+        item.classList.remove('focus');
+    }
+
+    // Focus on the section.
+    section.classList.add('focus');
+
+    // Focus on nav item.
+    const navItem = navigation.getItem(section.id);
+    navItem.classList.add('focus');
+}
+
+/**
  * @description Describes navigation list elements.
  * @constructor
  * @param {Element} container - Navigation list container.
@@ -8,13 +36,26 @@
 function Navigation(container, items) {
     this.container = container;
     this.items = items;
+
+    /** Retrieves a navigation item by section ID.
+     *
+     * @param {string} sectionId - ID of the section.
+     * @returns {element} - Navigation item with ID matching the section ID.
+     */
+    this.getItem = function(sectionId) {
+        for (const item of items) {
+            if (item.id === sectionId) {
+                return item;
+            }
+        }
+    };
 }
 
 /**
  * @description Build navigation list items based on given content.
  * @param {Element[]} contentSections - Build navigation based on this list of content sections.
  * @param {Element} navContainer - Created navigation list items are added as children to this container.
- * @returns {Navigation} Returns the newly created navigation list data.
+ * @returns {Navigation} - Returns the newly created navigation list data.
  */
 function buildNavigation(contentSections, navContainer) {
     const items = [];
@@ -22,12 +63,14 @@ function buildNavigation(contentSections, navContainer) {
 
         // Create nav li.
         const navItem = document.createElement('LI');
+        const sectionId = section.id;
+
         navItem.classList.add('nav-menu__item');
+        navItem.id = sectionId;
 
         // Create nav li a.
         const navAnchor = document.createElement('A');
         const sectionName = section.dataset.nav;
-        const sectionId = section.id;
 
         navAnchor.classList.add('nav-link');
         navAnchor.textContent = `${sectionName}`;
@@ -51,6 +94,11 @@ function pageSetup() {
     const contentSections = document.querySelectorAll('#content > .section');
 
     const nav = buildNavigation(contentSections, navContainer);
+
+    // Focus on the first section initially.
+    if (contentSections.length > 0) {
+        focusSection(contentSections[0], contentSections, nav);
+    }
 }
 
 pageSetup();
